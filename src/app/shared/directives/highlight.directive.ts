0e6fe8842690820919cd8633632eec4824bdf9ce
Gopalakrishnan Subramani <gopalakrishnan.subramani@gmail.com>
2021-04-27 14:31:23 +0530

    directives

--- src/app/shared/directives/highlight.directive.ts
+++ src/app/shared/directives/highlight.directive.ts
@@ -1,4 +1,5 @@
-import { Directive, OnInit, Input, HostListener } from '@angular/core';
+import { Directive, OnInit, Input, HostListener, 
+         ElementRef, Renderer2 } from '@angular/core';
 // Attribute directive
 // highlight the dom element when mouse enter 
 // directives doesn't have view/html
@@ -10,25 +11,34 @@ import { Directive, OnInit, Input, HostListener } from '@angular/core';
 })
 export class HighlightDirective implements OnInit {
 
-  @Input()
+  // now the property name is appHighlight, not color
+  @Input("appHighlight")
   color: string = 'lightgreen'
 
-  constructor() {
+  // hostElement is the actual dom wrapper that host directive
+  constructor(private hostElement: ElementRef, 
+              private renderer: Renderer2) {
     console.log("Highlight directive created")
   }
 
   ngOnInit() {
     console.log('highlight directive oninit', this.color)
+    console.log("host tag ", this.hostElement.nativeElement.tagName)
   }
 
   @HostListener("mouseenter")
   onMouseEnter() {
     console.log("mouse enter")
+    this.renderer.setStyle(this.hostElement.nativeElement,
+                           "background",
+                           this.color);
   }
 
   @HostListener("mouseleave")
   onMouseLeave() {
     console.log("mouse leave")
+    this.renderer.removeStyle(this.hostElement.nativeElement,
+      "background");
   }
 
 }
