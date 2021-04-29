04972e4a101369551ef3d5839b72aad949a7dd79
Gopalakrishnan Subramani <gopalakrishnan.subramani@gmail.com>
2021-04-29 12:28:28 +0530

    ngondestroy and unsusbcre

--- src/app/product/components/product-list/product-list.component.ts
+++ src/app/product/components/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { CartItem } from 'src/app/cart/models/cart-item';
 import { CartService } from 'src/app/cart/services/cart.service';
 import { Product } from '../../models/product';
@@ -9,8 +10,9 @@ import { ProductService } from '../../services/product.service';
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.scss']
 })
-export class ProductListComponent implements OnInit {
+export class ProductListComponent implements OnInit, OnDestroy {
   products: Product[];
+  subscription: Subscription;
 
   constructor(private productService: ProductService, 
               private cartService: CartService) { }
@@ -19,8 +21,15 @@ export class ProductListComponent implements OnInit {
     this.fetchProducts();
   }
 
+  ngOnDestroy() {
+    console.log("ProductList onDestroy")
+    if (this.subscription && !this.subscription.closed) {
+      this.subscription.unsubscribe(); // abort the pending call if any
+    }
+  }
+
   fetchProducts() {
-    this.productService.getProducts()
+    this.subscription = this.productService.getProducts()
       .subscribe( products => {
         console.log("Got products", products)
         this.products = products;
