baa1625d6d4083af0003eb55feeaafdd9f1116be
Gopalakrishnan Subramani <gopalakrishnan.subramani@gmail.com>
2021-04-30 12:28:57 +0530

    effect intro async

--- src/app/cart/components/checkout/checkout.component.ts
+++ src/app/cart/components/checkout/checkout.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Favorite } from 'src/app/models/favorite';
+import { checkout } from '../../actions/checkout.actions';
+import { Order } from '../../models/order';
+import { CartService } from '../../services/cart.service';
 
 @Component({
   selector: 'app-checkout',
@@ -7,9 +12,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CheckoutComponent implements OnInit {
 
-  constructor() { }
+  constructor(private store: Store<{favorites: Favorite[]}>, 
+              private cartService: CartService) { }
 
   ngOnInit(): void {
+  }
+
+  checkout() {
+    const order = new Order()
+    order.name = 'someone'
+    order.email = 'someone@example.com'
+    order.items = this.cartService.cartItems;
+    
+    const action = checkout({order: order})
+    this.store.dispatch(action)
+
+    // dispatch checkout action, we expect the effect to be called
   }
 
 }
