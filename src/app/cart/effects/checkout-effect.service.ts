ec864c2df5947a20d9f5399d94de3f95a825964f
Gopalakrishnan Subramani <gopalakrishnan.subramani@gmail.com>
2021-04-30 13:00:13 +0530

    check in url

--- src/app/cart/effects/checkout-effect.service.ts
+++ src/app/cart/effects/checkout-effect.service.ts
@@ -4,6 +4,8 @@ import { Injectable } from '@angular/core';
 import {Actions, createEffect, ofType} from '@ngrx/effects';
 import {map, mergeMap, catchError} from 'rxjs/operators';
 import {EMPTY} from 'rxjs';
+import { of } from 'rxjs';
+
 import { CheckoutService } from '../service/checkout.service';
 import { CartService } from '../services/cart.service';
 
@@ -17,26 +19,42 @@ export class CheckoutEffectService {
               private cartService: CartService) {
 
     console.log("Effect service created");
-    this.actions$.pipe(
-      ofType ('[Cart] checkout'), // filter only allow actions of certain type
-      map( (action: any) => {
-        console.log("checkout action ", action);
 
-        // async code, this is why we need effect
-        this.checkoutService.postOrder(action.order)
-            .subscribe ( (result) => {
-              console.log("posted order ", result)
-              this.cartService.empty(); // side effect
-            })
+    // this.actions$.pipe(
+    //   ofType ('[Cart] checkout'), // filter only allow actions of certain type
+    //   map( (action: any) => {
+    //     console.log("checkout action ", action);
 
-        return action;
-      })
-    )
-    .subscribe( () => {
-       console.log("Subscribe")
-    })
+    //     // async code, this is why we need effect
+    //     this.checkoutService.postOrder(action.order)
+    //         .subscribe ( (result) => {
+    //           console.log("posted order ", result)
+    //           this.cartService.empty(); // side effect
+    //         })
+
+    //     return action;
+    //   })
+    // )
+    // .subscribe( () => {
+    //    console.log("Subscribe")
+    // })
 
    }
+
+
+   
+  checkoutMovies$ = createEffect(() =>
+  this.actions$.pipe(
+    ofType('[Cart] checkout'),
+    mergeMap(( action: any) => this.checkoutService.postOrder(action.order)
+      .pipe(
+        map(order => ({ type: '[Cart] orderPlaced', payload: order })),
+        catchError(() => of({ type: '[Cart] checkoutFailed' }))
+      )
+    )
+  )
+);
+
 
    
 }
